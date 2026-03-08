d7c7503e45411a8bf0528cbd3d8177d6151f515c
Erick Busato <eabusato@gmail.com>
2026-03-07 23:14:07 -0300

    Phase 19 Implementation (new example)

--- tests/integration/asm_invalid_libc_symbol_16c2.cgen.s
+++
@@ -1,9 +0,0 @@
-	.intel_syntax noprefix
-	.text
-	.globl cct_fn_bad_libc
-cct_fn_bad_libc:
-	push ebp
-	mov ebp, esp
-	call printf
-	leave
-	ret
--- tests/integration/asm_invalid_no_intel_header_16c2.cgen.s
+++
@@ -1,7 +0,0 @@
-	.text
-	.globl cct_fn_bad_no_intel
-cct_fn_bad_no_intel:
-	push ebp
-	mov ebp, esp
-	leave
-	ret
